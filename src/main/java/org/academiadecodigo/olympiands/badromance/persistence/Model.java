84bf0b5e54f4032980a09f46949eb48b4d09459f
joseph-nun3z <joseph.nunez912@gmail.com>
2019-12-05 14:17:22 +0000

    constructing model

---
+++ src/main/java/org/academiadecodigo/olympiands/badromance/persistence/Model.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.olympiands.badromance.persistence;
+
+public interface Model {
+
+    Integer getID();
+
+    void setID(Integer id);
+}
---
+++ src/main/java/org/academiadecodigo/olympiands/badromance/service/LoginService.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.olympiands.badromance.service;
+
+public class LoginService {
+}
